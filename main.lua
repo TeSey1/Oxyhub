780e2784b4bea094651129facc99c204e97dcecc
TeSey1 <76228721+TeSey1@users.noreply.github.com>
2024-10-20 19:20:32 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -51,27 +51,15 @@ function autoupgrades()
 end
 
 function showpets()
-	if _G.showpets == false then
-		local args = {
-    		[1] = "ShowOtherPets",
-    		[2] = "PetSFX",
-    		[3] = "PetAuras",
-    		[4] = "FireworkShow"
-		}
-        for i = 1, #args do
-            game:GetService("ReplicatedStorage").Network:FindFirstChild("Toggle Setting"):InvokeServer(args[i])
-        end
-    else 
-        local args = {
-    		[1] = "ShowOtherPets",
-    		[2] = "PetSFX",
-    		[3] = "PetAuras",
-    		[4] = "FireworkShow"
-		}
-        for i = 1, #args do
-            game:GetService("ReplicatedStorage").Network:FindFirstChild("Toggle Setting"):InvokeServer(args[i])
-        end
-	end
+    local args = {
+        [1] = "ShowOtherPets",
+        [2] = "PetSFX",
+        [3] = "PetAuras",
+        [4] = "FireworkShow"
+    }
+    for i = 1, #args do
+        game:GetService("ReplicatedStorage").Network:FindFirstChild("Toggle Setting"):InvokeServer(args[i])
+    end
 end
 
 -----------------------------------------------
