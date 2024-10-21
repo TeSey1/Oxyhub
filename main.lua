22e89122112322a3ebe06252b346d2436b5b58b9
TeSey1 <76228721+TeSey1@users.noreply.github.com>
2024-10-21 18:29:26 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -82,29 +82,23 @@ function showDetails()
 end
 
 function hidePets()
-    local petsFolder = game.Workspace:WaitForChild("__THINGS"):WaitForChild("Pets")  -- Папка с питомцами
-    local storage = game.ReplicatedStorage:FindFirstChild("Pets")  -- Папка для хранения скрытых объектов -- Если папка для хранения не найдена, создаем ее
-    if not storage then
-        storage = Instance.new("Folder")
-        storage.Name = "Pets"
-        storage.Parent = game.ReplicatedStorage
-    end -- Перемещаем объекты в папку хранения
-    for _, obj in ipairs(petsFolder:GetChildren()) do 
-        obj.Parent = storage  -- Перемещаем объект в ReplicatedStorage
+    local petsFolder = game.Workspace:WaitForChild("__THINGS"):WaitForChild("Pets")  -- Папка с деталями карты
+    local storage = game.ReplicatedStoragePets  -- Папка для хранения скрытых объектов
+    for _, obj in ipairs(petsFolder:GetChildren()) do obj.Parent = storage  -- Перемещаем объект в ReplicatedStorage
     end
     print("Питомцы скрыты.")
 end
 
 function showPets()
-    local petsFolder = game.Workspace:WaitForChild("__THINGS"):WaitForChild("Pets")  -- Папка с питомцами
-    local storage = game.ReplicatedStorage:WaitForChild("Pets")  -- Папка для хранения скрытых объектов -- Восстанавливаем объекты из папки хранения
+    local petsFolder = game.Workspace:WaitForChild("__THINGS"):WaitForChild("Pets")  -- Папка с деталями карты
+    local storage = game.ReplicatedStoragePets  -- Папка для хранения скрытых объектов
     for _, obj in ipairs(storage:GetChildren()) do
         if obj:IsA("Model") and obj.Name:find("Pet") then  -- Проверяем, является ли объект моделью питомца
             obj.Parent = petsFolder  -- Перемещаем объекты обратно в Pets
-        end end
+        end
+    end
     print("Питомцы восстановлены.")
 end
-
 
 
 -----------------------------------------------
